fa2234d3c91f10151e34ed71d612fa9de2fcb453
Shoxrux71 <amirjonovdamin@gmail.com>
2023-06-06 23:51:18 +0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import './logic/counter/counter_cubit.dart';
+import './logic/counter/counter_state.dart';
+// import 'package:todo_cubit/presentation/screens/other_screens.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,87 +17,84 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      create: (ctx) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key, required this.title});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
-  int _counter = 0;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-  final StreamController _stremController = StreamController<int>();
-
-  void _incrementCounter() {
-    _stremController.sink.add(_counter++);
-    // setState(() {
-    //   _counter++;
-    // });
-  }
-
-  Stream<int> incrementStream() async* {
-    for (var i = 0; i < 10; i++) {
-      await Future.delayed(const Duration(seconds: 1));
-      yield i;
-    }
-  }
-
-  Future<int?> incrementFuture() async {
-    for (var i = 0; i < 10; i++) {
-      await Future.delayed(const Duration(seconds: 1));
-      return i;
-    }
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
+  // Stream<int> incrementStream() async* {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              BlocListener<CounterCubit, CounterState>(
+                listener: (context, state) {
+                  if (state is CounterError) {
+                    showDialog(
+                      context: context,
+                      builder: (ctx) => AlertDialog(
+                        title: Text(state.message),
+                      ),
+                    );
+                  }
+                },
+                child: BlocBuilder<CounterCubit, CounterState>(
+                  builder: (context, state) {
+                    if (state is CunterLoading) {
+                      return const CircularProgressIndicator();
+                    }
+                    return Text(
+                      '${state.number}',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            FloatingActionButton(
+              onPressed: context.read<CounterCubit>().increment,
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
             ),
-            StreamBuilder(
-              initialData: 0,
-              stream: incrementStream(),
-              builder: (context, snapShotlar) {
-                return Text(
-                  '${snapShotlar.data}',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                );
-              },
-            ),
-            FutureBuilder(
-              initialData: 0,
-              future: incrementFuture(),
-              builder: (context, snapShotlar) {
-                return Text(
-                  '${snapShotlar.data}',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                );
-              },
+            FloatingActionButton(
+              onPressed: context.read<CounterCubit>().decrement,
+              tooltip: 'decrement',
+              child: const Icon(Icons.remove),
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        ) // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
